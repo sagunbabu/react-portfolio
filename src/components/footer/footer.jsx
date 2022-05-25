1d0c34e30bcbdac28c9a4911eeeb422e3389b34a
sagun <sagunbabu@yahoo.com>
2022-05-26 01:16:13 +0530

    footer updated

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './footer.css'
 import { FaInstagram } from 'react-icons/fa'
-import { AiFillGithub } from 'react-icons/ai'
+// import { AiFillGithub } from 'react-icons/ai'
 import { AiFillLinkedin } from 'react-icons/ai'
 import { FaDribbble } from 'react-icons/fa'
 
@@ -21,7 +21,7 @@ const footer = () => {
 
             <div className="footer__socials">
                 <a href="https://instagram.com/sagun__babu"><FaInstagram /></a>
-                <a href="https://github.com/sagunbabu"><AiFillGithub /></a>
+                {/* <a href="https://github.com/sagunbabu"><AiFillGithub /></a> */}
                 <a href="https://www.linkedin.com/in/sagunbabu/"><AiFillLinkedin /></a>
                 <a href="https://dribble.com/sagunbabu"><FaDribbble /></a>
             </div>
